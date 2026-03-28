3cbfa0dd736a8d6f8ca14ee4b08f37575dafddc5
Anit <anit@Priyeshs-MacBook-Pro.local>
2026-03-28 13:34:55 +0530

    model data from csv file

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -1,3 +1,38 @@
-const planets = []
+const fs = require('fs');
+const { parse } = require('csv-parse');
+const path = require('path');
 
-module.exports = planets;
+const result = [];
+
+function isHabitable(planet) {
+  return (
+    planet["koi_disposition"] === "CONFIRMED" &&
+    planet["koi_insol"] > 0.36 &&
+    planet["koi_insol"] < 1.11 &&
+    planet["koi_prad"] < 1.6
+  );
+}
+const filePath = path.join(__dirname,'../','data','kepler.csv');
+
+fs.createReadStream(filePath)
+  .pipe(
+    parse({
+      comment: "#",
+      columns: true,
+    }),
+  )
+  .on("data", (res) => {
+    if (isHabitable) {
+      result.push(res);
+    }
+  })
+  .on("error", (error) => {
+    console.log(error);
+  })
+  .on("end", () => {
+    console.log(`We found ${result.length} habitable planets`);
+  });
+
+  module.exports ={
+    planets:result
+  }
