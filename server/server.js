b0407e85849c7a68cd1fd2be70a6ded3869d907f
Anit <anit@Priyeshs-MacBook-Pro.local>
2026-03-28 12:33:24 +0530

    client and server changes

--- server/server.js
+++ server/server.js
@@ -1,13 +1,16 @@
-const express = require('express');
+const http  = require('http');
+const app = require('./src/app');
 
 const PORT = 6000 || process.env.PORT;
 
-const app = express();
+const server = http.createServer(app);
+
+
 
 app.get('/hello',(req,res)=>{
     res.status(200).json({msg:'Hello from server'})
 })
 
-app.listen(PORT,()=>{
+server.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`)
 })
