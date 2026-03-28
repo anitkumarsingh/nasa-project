f52dc2d4f064ff8fa3c13602eb0f9050955b3389
Anit <anit@Priyeshs-MacBook-Pro.local>
2026-03-28 13:10:03 +0530

    cors handing

--- server/src/app.js
+++ server/src/app.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 const planetRouter = require('./routes/planets/planets.router');
 
 const app = express();
+
+app.use(cors({
+    origin:'http://localhost:5173'
+}));
+
 app.use(express.json());
 app.use('/planets',planetRouter);
 
